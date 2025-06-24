db75af740289c769e627ce3240a06d0fdf8d8f13
Nataliia Klymenko <Natalia@bluedot.io>
2025-06-24 19:13:14 +1000

    Initial version of SwiftUI app

---
+++ AppDelegate.swift
@@ -0,0 +1,33 @@
+//
+//  AppDelegate.swift
+//  PointSDKMinimal
+//
+//  Created by Nataliia Klymenko on 13/6/2025.
+//  Copyright © 2025 Bluedot Innovation. All rights reserved.
+//
+
+import UIKit
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        requestNotificationPermissions()
+        
+        return true
+    }
+    
+    func requestNotificationPermissions() {
+        Task {
+            let center = UNUserNotificationCenter.current()
+            do {
+                if try await center.requestAuthorization(options: [.alert, .sound]) == true {
+                    print("Request Notification Permissions - Success")
+                } else {
+                    print("Request Notification Permissions - Success")
+                }
+                
+            } catch {
+                print("Request Notification Permissions - Error")
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# PointSDK-Minimal-Integration-iOS
-Demonstrates the Point SDK integrated into SwiftUI application
+# Point SDK - Minimal Integration App Example
+This project demonstrates the recommended integration approach for the iOS Point SDK using a SwiftUI application as an example.
+To use this example project, you first need to have an active Bluedot account and use the credentials. To request for a demo, please visit https://bluedot.io/demo/.
+
+### Get started
+
+1. Open the PointSDKMinimal.xcodeproj in Xcode.
+3. Then, compile and run the project in either simulator or device.
+4. Paste your Project ID from Canvas to MainViewModel (Canvas > Projects > Project ID)
+5. Use a Destination ID from Canvas to enable Tempo tracking (Canvas > Stores > Store Information > Destination ID)
+
+Please refer to the documentation at http://docs.bluedot.io
